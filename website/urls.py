9c45c0b898b40f83a0656e6d2f10956be3c94084
ira-ngd <ira.n.dg80@gmail.com>
2025-08-12 19:58:58 +0330

    website app created
    website templates & base.html
    website static

---
+++ website/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from website.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name='website'
+
+urlpatterns = [
+    path('',index_view,name='index'),
+    path('about',about_view,name='about'),
+    path('contact',contact_view,name='contact'),
+]
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
