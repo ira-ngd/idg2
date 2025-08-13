54f26ed91ad7f6816dd590f6625d0271097291c2
ira-ngd <ira.n.dg80@gmail.com>
2025-08-13 10:08:22 +0330

    blog added
    blog index
    blog single

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from website.views import *
-from django.conf import settings
-from django.conf.urls.static import static
 
 app_name='website'
 
@@ -10,5 +8,3 @@ urlpatterns = [
     path('about',about_view,name='about'),
     path('contact',contact_view,name='contact'),
 ]
-urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
