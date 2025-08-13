8b5acbb0c631eac243adcd383f243d37fa2603f3
ira-ngd <ira.n.dg80@gmail.com>
2025-08-13 17:28:53 +0330

    make ready for render.com

--- idg2/settings.py
+++ idg2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-v4ui3p-k1=)%mlfqcr^yd^rg0ci@c@wkk-6%7-)$g*@3^ewz)h"
+SECRET_KEY = os.environ.get("SECRET_KEY")
+#Old: "django-insecure-v4ui3p-k1=)%mlfqcr^yd^rg0ci@c@wkk-6%7-)$g*@3^ewz)h"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
